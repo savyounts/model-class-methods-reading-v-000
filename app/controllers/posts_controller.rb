8fe8f3efd82ea460d92df541f0409f7ca532caf1
Savannah Younts <savyounts@gmail.com>
2018-09-26 16:16:38 +0000

    automatically backed up by learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,15 +4,15 @@ class PostsController < ApplicationController
     @authors = Author.all
 
     if !params[:author].blank?
-    @posts = Post.where(author: params[:author])
-  elsif !params[:date].blank?
-    if params[:date] == "Today"
-      @posts = Post.where("created_at >=?", Time.zone.today.beginning_of_day)
+      @posts = Post.where(author: params[:author])
+    elsif !params[:date].blank?
+      if params[:date] == "Today"
+        @posts = Post.where("created_at >=?", Time.zone.today.beginning_of_day)
+      else
+        @posts = Post.where("created_at <?", Time.zone.today.beginning_of_day)
+      end
     else
-      @posts = Post.where("created_at <?", Time.zone.today.beginning_of_day)
-    end
-  else
-      @posts = Post.all
+        @posts = Post.all
     end
   end
 
